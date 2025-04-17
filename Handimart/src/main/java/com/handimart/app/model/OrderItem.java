09ca95c147272a66addd3662fb17eb57166eadc1
Sathmi1234 <devsadini@gmail.com>
2025-04-17 13:40:11 +0530

    feat: adding validations to OrderItem model

--- Handimart/src/main/java/com/handimart/app/model/OrderItem.java
+++ Handimart/src/main/java/com/handimart/app/model/OrderItem.java
@@ -7,6 +7,12 @@ import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 
 @Entity
 @Table(name = "order_items")
@@ -14,10 +20,25 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class OrderItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long orderItemId;
+    
+    @ManyToOne
+    @JoinColumn(name = "order_id", nullable = false)
     private Order order;
+    
+    @ManyToOne
+    @JoinColumn(name = "product_id", nullable = false)
     private Product product;
+    
+    @Column(nullable = false)
     private Integer quantity;
+    
+    @Column(nullable = false, precision = 10, scale = 2)
     private BigDecimal unitPrice;
+    
+    @Column(nullable = false, precision = 10, scale = 2)
     private BigDecimal subtotal;
 }
