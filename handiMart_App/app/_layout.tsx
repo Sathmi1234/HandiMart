7316410593ccd5275655b152457c5761f3c94794
5yndr0m <sdilanjana21@gmail.com>
2025-04-17 22:10:11 +0530

    feat: set up blobal status bar

--- handiMart_App/app/_layout.tsx
+++ handiMart_App/app/_layout.tsx
@@ -1,10 +1,16 @@
 import { Stack } from "expo-router";
+import { StatusBar } from "expo-status-bar";
 import { PaperProvider } from "react-native-paper";
+
 
 export default function RootLayout() {
   return (
     <PaperProvider>
-      <Stack />
+      <StatusBar style="auto"  />
+      <Stack screenOptions={{headerShown: false}}>
+        
+        
+      </Stack>
     </PaperProvider>
   );
 }
