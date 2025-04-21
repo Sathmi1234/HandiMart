93299ea55aacded8d36d398317256f7489d4923d
5yndr0m <sdilanjana21@gmail.com>
2025-04-21 22:13:43 +0530

    feat: setting up authentications

---
+++ Handimart/src/main/java/com/handimart/app/config/AppConfig.java
@@ -0,0 +1,31 @@
+package com.handimart.app.config;
+
+import org.springframework.boot.autoconfigure.integration.IntegrationProperties.Management;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+public class AppConfig {
+
+	@Bean
+	SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+		
+		http.sessionManagement(Management -> Management.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+			.authorizeHttpRequests(Authorize -> Authorize
+					.requestMatchers("/api/admin/**").hasAnyRole("Seller")
+					.requestMatchers("/api/**").authenticated()
+					.anyRequest().permitAll()
+			).addFilterBefore(new JwtTokenValidator(), BasicAuthenticationFilter.class)
+			.csrf(csrf->csrf.disable())
+			.cors(cors->cors.configurationSource(corsConfigurationSource()));
+			
+			
+		return null;
+	}
+}
