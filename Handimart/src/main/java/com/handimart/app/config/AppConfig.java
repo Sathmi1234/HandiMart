329e6ffe81ffbfe44ba93e45a4f1a1225c89945e
5yndr0m <sdilanjana21@gmail.com>
2025-04-24 20:29:03 +0530

    feat: implement passwordvalidator

--- Handimart/src/main/java/com/handimart/app/config/AppConfig.java
+++ Handimart/src/main/java/com/handimart/app/config/AppConfig.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
@@ -58,7 +59,8 @@ public class AppConfig {
 		};
     }
 
+	@Bean
 	PasswordEncoder passwordEncoder(){
-		return new b
+		return new BCryptPasswordEncoder();
 	}
 }
