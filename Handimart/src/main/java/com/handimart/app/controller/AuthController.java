8f56c3ec72beec866e20643812efc8d773633117
5yndr0m <sdilanjana21@gmail.com>
2025-05-13 21:37:03 +0530

    feat: authenticate controller with signup and sign in methods

---
+++ Handimart/src/main/java/com/handimart/app/controller/AuthController.java
@@ -0,0 +1,117 @@
+package com.handimart.app.controller;
+
+import com.handimart.app.config.JwtProvider;
+import com.handimart.app.model.Cart;
+import com.handimart.app.model.USER_ROLE;
+import com.handimart.app.model.User;
+import com.handimart.app.repository.CartRepository;
+import com.handimart.app.repository.UserRepository;
+import com.handimart.app.request.LoginRequest;
+import com.handimart.app.response.AuthResponse;
+import com.handimart.app.service.CustomerUserDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collection;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Autowired
+    private JwtProvider jwtProvider;
+
+    @Autowired
+    private CustomerUserDetailsService customerUserDetailsService;
+
+    @Autowired
+    private CartRepository cartRepository;
+
+    @PostMapping("/signup")
+    public ResponseEntity<AuthResponse> createUserHandler(@RequestBody User user) throws Exception {
+
+        User isEmailExist = userRepository.findByEmail(user.getEmail());
+        if(isEmailExist != null){
+            throw new Exception("Email is already in use");
+        }
+
+        User createdUser = new User();
+        createdUser.setUsername(user.getUsername());
+        createdUser.setEmail(user.getEmail());
+        createdUser.setFirst_name(user.getFirst_name());
+        createdUser.setLast_name(user.getLast_name());
+        createdUser.setRole(user.getRole());
+        createdUser.setPassword(passwordEncoder.encode(user.getPassword()));
+
+        User savedUser = userRepository.save(createdUser);
+
+        Cart cart = new Cart();
+        cart.setCartOwner(savedUser);
+        cartRepository.save(cart);
+
+        Authentication authentication = new UsernamePasswordAuthenticationToken(user.getEmail(), user.getPassword());
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        String jwt = jwtProvider.generateToke(authentication);
+
+        AuthResponse authResponse = new AuthResponse();
+        authResponse.setJwt(jwt);
+        authResponse.setMessage("Register success");
+        authResponse.setRole(savedUser.getRole());
+
+        return new ResponseEntity<>(authResponse, HttpStatus.CREATED);
+    }
+
+    @PostMapping("/signin")
+    public ResponseEntity<AuthResponse> signin(@RequestBody LoginRequest req){
+
+        String username = req.getEmail();
+        String password = req.getPassword();
+
+        Authentication authentication = authenticate(username, password);
+
+        Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
+        String role = authorities.isEmpty()?null: authorities.iterator().next().getAuthority();
+
+        String jwt = jwtProvider.generateToke(authentication);
+
+        AuthResponse authResponse = new AuthResponse();
+        authResponse.setJwt(jwt);
+        authResponse.setMessage("Login success");
+        authResponse.setRole(USER_ROLE.valueOf(role));
+
+        return new ResponseEntity<>(authResponse, HttpStatus.OK);
+    }
+
+    private Authentication authenticate(String username, String password) {
+
+        UserDetails userDetails = customerUserDetailsService.loadUserByUsername(username);
+        if(userDetails == null){
+            throw new BadCredentialsException("Invalid username");
+        }
+
+        if(!passwordEncoder.matches(password, userDetails.getPassword())){
+            throw new BadCredentialsException("Invalid password");
+        }
+
+        return new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+    }
+}
