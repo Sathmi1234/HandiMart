50e147982cac6a158c4f787af1ea2b3c795137e3
5yndr0m <sdilanjana21@gmail.com>
2025-05-19 22:05:15 +0530

    feat: create seller repository

--- Handimart/src/main/java/com/handimart/app/repository/SellerRepository.java
+++ Handimart/src/main/java/com/handimart/app/repository/SellerRepository.java
@@ -1,4 +1,12 @@
 package com.handimart.app.repository;
 
-public interface SellerRepository {
+import com.handimart.app.model.SellerProfile;
+import com.handimart.app.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SellerRepository extends JpaRepository<SellerProfile, Long> {
+
+    public SellerProfile findByUser(User user);
 }
