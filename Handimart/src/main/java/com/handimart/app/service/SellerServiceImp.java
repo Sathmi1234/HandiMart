87c309895429e48379a17a22a7c2d4f3f3b70abe
5yndr0m <sdilanjana21@gmail.com>
2025-05-19 22:05:59 +0530

    feat: creating and adding seller services

--- Handimart/src/main/java/com/handimart/app/service/SellerServiceImp.java
+++ Handimart/src/main/java/com/handimart/app/service/SellerServiceImp.java
@@ -1,4 +1,34 @@
 package com.handimart.app.service;
 
-public class SellerServiceImp {
+import com.handimart.app.config.JwtProvider;
+import com.handimart.app.model.SellerProfile;
+import com.handimart.app.model.User;
+import com.handimart.app.repository.SellerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SellerServiceImp implements SellerService{
+
+    @Autowired
+    private SellerRepository sellerRepository;
+
+    @Autowired
+    private JwtProvider jwtProvider;
+
+    @Override
+    public SellerProfile findUserByJwtToken(String jwt) throws Exception {
+        return null;
+    }
+
+    @Override
+    public SellerProfile findUseryUser(User target) throws Exception {
+        SellerProfile seller = sellerRepository.findByUser(target);
+
+        if(seller == null){
+            throw new Exception("User not found");
+        }
+
+        return seller;
+    }
 }
