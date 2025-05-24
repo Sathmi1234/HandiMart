ff0dec366afd65ff06370eff3e25281378052183
5yndr0m <sdilanjana21@gmail.com>
2025-05-24 10:55:27 +0530

    feat: add wishlist item request and response

--- Handimart/src/main/java/com/handimart/app/request/WishListItemRequest.java
+++ Handimart/src/main/java/com/handimart/app/request/WishListItemRequest.java
@@ -1,4 +1,9 @@
 package com.handimart.app.request;
 
+import lombok.Data;
+
+@Data
 public class WishListItemRequest {
+    private Long wishListId;
+    private Long productId;
 }
--- Handimart/src/main/java/com/handimart/app/response/WishListItemResponse.java
+++ Handimart/src/main/java/com/handimart/app/response/WishListItemResponse.java
@@ -1,4 +1,14 @@
 package com.handimart.app.response;
 
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
 public class WishListItemResponse {
+    private Long wishListItemId;
+    private Long wishListId;
+    private Long productId;
+    private String productName;
+    private LocalDateTime addedAt;
 }
