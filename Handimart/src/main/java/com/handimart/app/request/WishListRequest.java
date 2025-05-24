bd210ab1d58a06e843e84b2823002267f0df8b14
5yndr0m <sdilanjana21@gmail.com>
2025-05-24 10:55:12 +0530

    feat: add wishlist request and response

--- Handimart/src/main/java/com/handimart/app/request/WishListRequest.java
+++ Handimart/src/main/java/com/handimart/app/request/WishListRequest.java
@@ -1,4 +1,8 @@
 package com.handimart.app.request;
 
+import lombok.Data;
+
+@Data
 public class WishListRequest {
+    private Long user_id;
 }
--- Handimart/src/main/java/com/handimart/app/response/WishListResponse.java
+++ Handimart/src/main/java/com/handimart/app/response/WishListResponse.java
@@ -1,4 +1,13 @@
 package com.handimart.app.response;
 
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
 public class WishListResponse {
+    private Long wishListId;
+    private Long userId;
+    private LocalDateTime createdAt;
+
 }
