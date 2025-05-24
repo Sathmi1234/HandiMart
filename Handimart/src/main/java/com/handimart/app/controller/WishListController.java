93f289216d5bc15419222e4abf9d494f6b1e4329
5yndr0m <sdilanjana21@gmail.com>
2025-05-24 10:56:23 +0530

    feat: add wishlist controllers

--- Handimart/src/main/java/com/handimart/app/controller/WishListController.java
+++ Handimart/src/main/java/com/handimart/app/controller/WishListController.java
@@ -1,4 +1,25 @@
 package com.handimart.app.controller;
 
+import com.handimart.app.request.WishListRequest;
+import com.handimart.app.response.WishListResponse;
+import com.handimart.app.service.WishListService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/wishlist")
 public class WishListController {
+
+    @Autowired
+    private WishListService wishListService;
+
+    @PostMapping
+    public WishListResponse createWishList(@RequestBody WishListRequest request){
+        return wishListService.createWishList(request);
+    }
+
+    @GetMapping("/user/{userId}")
+    public WishListResponse getWishListByUserId(@PathVariable("userId") Long user_id){
+        return wishListService.getWishListByUserId(user_id);
+    }
 }
