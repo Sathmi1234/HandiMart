4bfae4108ecd03b1fb7f3fa386872f2abebf082f
5yndr0m <sdilanjana21@gmail.com>
2025-05-24 22:14:30 +0530

    feat: add order and order item responses

---
+++ Handimart/src/main/java/com/handimart/app/response/OrderItemResponse.java
@@ -0,0 +1,61 @@
+package com.handimart.app.response;
+
+import java.math.BigDecimal;
+
+import lombok.Data;
+
+@Data
+public class OrderItemResponse {
+
+	private Long orderItemId;
+	private Long productId;
+	private Integer quantity;
+	private BigDecimal unitPrice;
+	private BigDecimal subTotal;
+	
+	public OrderItemResponse() {
+		
+	}
+
+	public Long getOrderItemId() {
+		return orderItemId;
+	}
+
+	public void setOrderItemId(Long orderItemId) {
+		this.orderItemId = orderItemId;
+	}
+
+	public Long getProductId() {
+		return productId;
+	}
+
+	public void setProductId(Long productId) {
+		this.productId = productId;
+	}
+
+	public Integer getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(Integer quantity) {
+		this.quantity = quantity;
+	}
+
+	public BigDecimal getUnitPrice() {
+		return unitPrice;
+	}
+
+	public void setUnitPrice(BigDecimal unitPrice) {
+		this.unitPrice = unitPrice;
+	}
+
+	public BigDecimal getSubTotal() {
+		return subTotal;
+	}
+
+	public void setSubTotal(BigDecimal subTotal) {
+		this.subTotal = subTotal;
+	}
+	
+	
+}
