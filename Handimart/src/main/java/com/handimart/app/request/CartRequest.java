3432ae8d02c2043901382fb3daa4018de7e8da70
5yndr0m <sdilanjana21@gmail.com>
2025-05-29 06:09:45 +0530

    feat: create requests

---
+++ Handimart/src/main/java/com/handimart/app/request/CartRequest.java
@@ -0,0 +1,17 @@
+package com.handimart.app.request;
+
+public class CartRequest {
+	 private Long userId;
+
+	public Long getUserId() {
+		return userId;
+	}
+
+	public void setUserId(Long userId) {
+		this.userId = userId;
+	}
+	 
+	public CartRequest() {
+		
+	}
+}
