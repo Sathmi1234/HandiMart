fab7aa2151eb0a04e4f3f2bff97c74c1d11508dd
5yndr0m <sdilanjana21@gmail.com>
2025-05-29 06:09:30 +0530

    feat: create repositories

--- Handimart/src/main/java/com/handimart/app/repository/CartRepository.java
+++ Handimart/src/main/java/com/handimart/app/repository/CartRepository.java
@@ -1,7 +1,10 @@
 package com.handimart.app.repository;
 
 import com.handimart.app.model.Cart;
+import com.handimart.app.model.User;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CartRepository extends JpaRepository<Cart, Long> {
+	Cart findByCartOwner(User user);
 }
