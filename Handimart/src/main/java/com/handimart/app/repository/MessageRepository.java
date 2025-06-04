d6fb11ce93ed330710f03bb90921a34740d6c3e1
Sathmi1234 <devsadini@gmail.com>
2025-06-04 21:37:17 +0530

    feat: create MessageRepository interface for Message model persistence

---
+++ Handimart/src/main/java/com/handimart/app/repository/MessageRepository.java
@@ -0,0 +1,13 @@
+package com.handimart.app.repository;
+
+import com.handimart.app.model.Message;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+
+@Repository
+public interface MessageRepository extends JpaRepository<Message, Long> {
+    
+}
