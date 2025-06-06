8ad4740a70bc3291d1dd01915330b429a55f6ac7
5yndr0m <sdilanjana21@gmail.com>
2025-06-06 05:56:46 +0530

    fix: commenting this controller methods because partial implementations breaks the app

--- Handimart/src/main/java/com/handimart/app/controller/StripeWebhookController.java
+++ Handimart/src/main/java/com/handimart/app/controller/StripeWebhookController.java
@@ -17,29 +17,29 @@ import com.stripe.net.Webhook;
 @RequestMapping("/api/user/payment/webhook")
 public class StripeWebhookController {
 
-	@Value("${stripe.webhook.secret}")
-    private String endpointSecret;
-
-    private final TransactionService transactionService;
-
-    public StripeWebhookController(TransactionService transactionService) {
-        this.transactionService = transactionService;
-    }
-
-    @PostMapping
-    public ResponseEntity<String> handleStripeEvent(@RequestBody String payload,
-                                                    @RequestHeader("Stripe-Signature") String sigHeader) {
-        try {
-            Event event = Webhook.constructEvent(payload, sigHeader, endpointSecret);
-
-            if ("payment_intent.succeeded".equals(event.getType())) {
-                PaymentIntent paymentIntent = (PaymentIntent) event.getDataObjectDeserializer().getObject().get();
-                transactionService.handleStripeWebhook(paymentIntent);
-            }
-
-            return ResponseEntity.ok("Event processed");
-        } catch (Exception e) {
-            return ResponseEntity.badRequest().body("Webhook Error: " + e.getMessage());
-        }
-    }
+//	@Value("${stripe.webhook.secret}")
+//    private String endpointSecret;
+//
+//    private final TransactionService transactionService;
+//
+//    public StripeWebhookController(TransactionService transactionService) {
+//        this.transactionService = transactionService;
+//    }
+//
+//    @PostMapping
+//    public ResponseEntity<String> handleStripeEvent(@RequestBody String payload,
+//                                                    @RequestHeader("Stripe-Signature") String sigHeader) {
+//        try {
+//            Event event = Webhook.constructEvent(payload, sigHeader, endpointSecret);
+//
+//            if ("payment_intent.succeeded".equals(event.getType())) {
+//                PaymentIntent paymentIntent = (PaymentIntent) event.getDataObjectDeserializer().getObject().get();
+//                transactionService.handleStripeWebhook(paymentIntent);
+//            }
+//
+//            return ResponseEntity.ok("Event processed");
+//        } catch (Exception e) {
+//            return ResponseEntity.badRequest().body("Webhook Error: " + e.getMessage());
+//        }
+//    }
 }
