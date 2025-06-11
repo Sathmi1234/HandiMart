6fee1d9b76ebe3ee20d6c8853de06c8c13f34ea3
5yndr0m <sdilanjana21@gmail.com>
2025-06-11 16:17:27 +0530

    feat: add change password methods

---
+++ Handimart/src/main/java/com/handimart/app/request/ChangePasswordRequest.java
@@ -0,0 +1,30 @@
+package com.handimart.app.request;
+
+public class ChangePasswordRequest {
+
+	 private String oldPassword;
+	 private String newPassword;
+	public String getOldPassword() {
+		return oldPassword;
+	}
+	public void setOldPassword(String oldPassword) {
+		this.oldPassword = oldPassword;
+	}
+	public String getNewPassword() {
+		return newPassword;
+	}
+	public void setNewPassword(String newPassword) {
+		this.newPassword = newPassword;
+	}
+	 
+	 public ChangePasswordRequest() {
+		 
+	 }
+	public ChangePasswordRequest(String oldPassword, String newPassword) {
+		super();
+		this.oldPassword = oldPassword;
+		this.newPassword = newPassword;
+	}
+	 
+	 
+}
